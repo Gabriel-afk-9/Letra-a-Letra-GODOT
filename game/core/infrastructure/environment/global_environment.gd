c25ae3e47e10a8756533f144e30c0e20680226e2
Zidan-09 <samuel.profissional18@gmail.com>
2026-09-11 11:08:18 -0300

    fix: fixed login button

--- game/core/infrastructure/environment/global_environment.gd
+++ game/core/infrastructure/environment/global_environment.gd
@@ -1,4 +1,4 @@
 extends Node
 
-const API_BASE_URL = "http://localhost:8080"
-const WS_BASE_URL = "ws://localhost:8080/ws/game"
+const API_BASE_URL = "https://api.letraaletradev.qzz.io"
+const WS_BASE_URL = "wss://api.letraaletradev.qzz.io/ws/game"
